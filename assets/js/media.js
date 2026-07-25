22933331cde77422cb1e2ebd426553f33beb1391
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-25 22:29:30 +0330

    Proxy all real player portraits through a reliable image CDN

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,7 +1,10 @@
 export const MEDIA={
-  younes:'https://media.eintracht.de/image/upload/ar_1:1,c_fill,dpr_1.0,f_auto,g_xy_center,q_82,w_1000,x_w_mul_0.5,y_h_mul_0/hero_ebnoutalib-e338.png',
-  dickson:'https://assets.bundesliga.com/player/dfl-obj-002ge4-dfl-clu-00000i-dfl-sea-0001k9-body.png?crop=10%2C0%2C75%2C80&fit=900%2C1100',
-  jamie:'https://www.vfb.de/images/redaktion/images/profis/2425-Kader-Portraits/18-Leweling_00000_x_0x0.jpg',
+  younes:'https://images.weserv.nl/?url=https%3A%2F%2Fmedia.eintracht.de%2Fimage%2Fupload%2Far_1%3A1%2Cc_fill%2Cdpr_1.0%2Cf_auto%2Cg_xy_center%2Cq_82%2Cw_1000%2Cx_w_mul_0.5%2Cy_h_mul_0%2Fhero_ebnoutalib-e338.png&w=700&h=700&fit=cover&output=webp',
+  dickson:'https://images.weserv.nl/?url=https%3A%2F%2Fassets.bundesliga.com%2Fplayer%2Fdfl-obj-002ge4-dfl-clu-00000i-dfl-sea-0001k9-body.png%3Fcrop%3D10%252C0%252C75%252C80%26fit%3D900%252C1100&w=700&h=700&fit=cover&output=webp',
+  jamie:'https://images.weserv.nl/?url=https%3A%2F%2Fwww.vfb.de%2Fimages%2Fredaktion%2Fimages%2Fprofis%2F2425-Kader-Portraits%2F18-Leweling_00000_x_0x0.jpg&w=700&h=700&fit=cover&output=webp',
+  luca:'https://images.weserv.nl/?url=https%3A%2F%2Fassets.bundesliga.com%2Fplayer%2Fdfl-obj-j01iwm-dfl-clu-000n6l-dfl-sea-0001ka-body.png%3Fcrop%3D10%252C0%252C75%252C80%26fit%3D256%252C256&w=700&h=700&fit=cover&output=webp',
+  ayoub:'https://images.weserv.nl/?url=https%3A%2F%2Fs.hs-data.com%2Fgfx%2Fperson%2Fcropped%2F400x400%2F643923.png%3Ffallback%3Dmale&w=700&h=700&fit=cover&output=webp',
+  aymen:'https://images.weserv.nl/?url=https%3A%2F%2Fs.hs-data.com%2Fgfx%2Fperson%2Fcropped%2F400x400%2F347866.png%3Ffallback%3Dmale&w=700&h=700&fit=cover&output=webp',
   editorial:'https://images.unsplash.com/photo-1540747913346-19e32dc3e97e?auto=format&fit=crop&w=1400&q=88',
   stadium:'https://images.unsplash.com/photo-1574629810360-7efbbe195018?auto=format&fit=crop&w=1600&q=88',
   lifestyle:'https://images.unsplash.com/photo-1530549387789-4c1017266635?auto=format&fit=crop&w=1400&q=88',
